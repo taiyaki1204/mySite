91ad70bc3333a3b79c5365897d899f7f7c615dba
taiyaki <test@gmail.com>
2020-08-06 10:14:03 +0900

    practice1

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-hello
+
+//通常の関数
+const t1 = function sum1 (x,y) {return x + y};
+t1(1,2);
+
+//無名関数
+const t2 = function (x,y) {return x+y};
+t2(1,2);
+
+//アロー関数
+const t3 = (x,y) =>{return x+y};
+t3(1,2);
+
